332de8c88b600c5ef464462fd58006578bd28230
ümit sari <umitsari1986@hotmail.com>
2023-08-11 22:50:30 +0300

    Day36ay36_inheritanceDataTypeKullanimi5_inheritance

---
+++ src/Day36_inheritanceDataTypeKullanimi/DVariableRunner.java
@@ -0,0 +1,49 @@
+package Day36_inheritanceDataTypeKullanimi;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DVariableRunner {
+    public static void main(String[] args) {
+        CAvciKuslar avciKusKartal = new CAvciKuslar(); // data turu CAvciKuslar ve constructor CAvciKuslar()
+        System.out.println(avciKusKartal.hareket); // C Ucarlar
+        System.out.println(avciKusKartal.beslenme); // C et yer
+        System.out.println(avciKusKartal.pence); // C pencelidir
+        System.out.println(avciKusKartal.gaga); // C sivri gagalidir
+        System.out.println(avciKusKartal.kanat); // B kanatlidir
+        System.out.println(avciKusKartal.solunum); // B akciger
+        System.out.println(avciKusKartal.cogalma); // B Yumurtayla
+        System.out.println(avciKusKartal.omur); // A yasar ve olur
+
+        BKuslar kusKartal = new CAvciKuslar(); // data turu BKuslar ve constructor CAvciKuslar()
+        System.out.println(kusKartal.hareket); // A hareket eder
+        System.out.println(kusKartal.beslenme); // A beslenir
+        // System.out.println(kusKartal.pence); // B veya A'da yok CTE
+        System.out.println(kusKartal.gaga); // B gagalidir
+        System.out.println(kusKartal.kanat); // B kanatlidir
+        System.out.println(kusKartal.solunum); // B akciger
+        System.out.println(kusKartal.cogalma); // B Yumurtayla
+        System.out.println(kusKartal.omur); // A yasar ve olur
+
+        AHayvan hayvanKartal = new CAvciKuslar(); // data turu AHayvan ve constructor CAvciKuslar()
+        System.out.println(hayvanKartal.hareket); // A hareket eder
+        System.out.println(hayvanKartal.beslenme); // A Beslenir
+        // System.out.println(hayvanKartal.pence); // A'da yok CTE
+        // System.out.println(hayvanKartal.gaga); // A'da yok CTE
+        // System.out.println(hayvanKartal.kanat); // A'da yok CTE
+        System.out.println(hayvanKartal.solunum); // A nefes alir
+        System.out.println(hayvanKartal.cogalma); // A Cogalir
+        System.out.println(hayvanKartal.omur); // A yasar ve olur
+    }
+    /*
+        Java'da en klasik obje olusturma yontemi
+        ClassAdi  obj = new ClassAdi();
+        ama Child class'in constructor'i kullanilarak olusturulan objeler icin
+        data turu Parent class'lar olarak secilebilir
+        Eger constructor ve data turu farkli ise orn. AHayvan hayvanKartal = new CAvciKuslar();
+        objenin ozelliklerini aramaya DATA TURU olan  (AHayvan class'ından) class'dan baslanir
+        EGER ozellikler variable olarak olusturuldu ise
+        objenin ozelliklerini DATA TURU ( örnek :AHayvan data turu) belirler
+     */
+}
