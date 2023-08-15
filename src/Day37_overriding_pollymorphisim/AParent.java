a23b964833648e3abdfde35bf2ccb480a201a075
ümit sari <umitsari1986@hotmail.com>
2023-08-15 23:20:40 +0300

    Day37_overriding_pollymorphisim

---
+++ src/Day37_overriding_pollymorphisim/AParent.java
@@ -0,0 +1,13 @@
+package Day37_overriding_pollymorphisim;
+
+public class AParent {
+    public final void finalMethod(){
+        System.out.println("Bu bir final method");
+    }
+    public static void staticMethod(){
+        System.out.println("Bu bir static method");
+    }
+    private void privateMethod(){
+        System.out.println("Bu bir private method");
+    }
+}
