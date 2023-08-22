f41bbdbd2077a504437cb61546b925c23ffda499
ümit sari <umitsari1986@hotmail.com>
2023-08-22 10:59:13 +0300

    src/Day40_interfaces

---
+++ src/Day40_interfaces/C01_ChildOfInterfaces.java
@@ -0,0 +1,38 @@
+package Day40_interfaces;
+
+import Day39_interfaces.I01_Interfaces;
+import Day39_interfaces.I02_Interfaces;
+
+public class C01_ChildOfInterfaces implements I01_Interfaces, I02_Interfaces {
+
+
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public int method2() {
+        return 0;
+    }
+
+    @Override
+    public int method11() {
+        return 0;
+    }
+
+    @Override
+    public String method3() {
+        return null;
+    }
+
+    @Override
+    public void method44() {
+
+    }
+
+    @Override
+    public void method4() {
+
+    }
+}
